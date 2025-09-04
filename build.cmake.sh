5f31a4310537821ea8044bdd4cce1c3dbaa279f1
pvn <pedro.vicente@hushmail.com>
2025-09-04 12:00:21 -0400

    run example on Linux

--- build.cmake.sh
+++ build.cmake.sh
@@ -38,17 +38,30 @@ cmake --build .
 
 elif [[ "$OSTYPE" == "msys" ]]; then
 
-path_boost="$(pwd)/build/boost_1_88_0"
+path_boost="$(pwd)/boost_1_88_0"
 echo "Boost at: $path_boost"
 
-cmake .. --fresh \
+mkdir -p wt.extensions
+pushd wt.extensions
+
+cmake ../.. --fresh \
     -DWT_INCLUDE="$path_wt/src" \
     -DWT_CONFIG_H="$path_wt_build" \
     -DBOOST_INCLUDE_DIR="$path_boost/include/boost-1_88" \
-    -DBOOST_LIB_DIRS="$path_boost/stage/lib/"
+    -DBOOST_LIB_DIRS="$path_boost/lib"
 cmake --build .  --config Debug --parallel 
+
+popd
 
 fi
 
 popd
 
+
+if [[ "$OSTYPE" != "msys" ]]; then
+pushd build
+export LD_LIBRARY_PATH=$path_boost/stage/lib:$LD_LIBRARY_PATH
+echo "open browser http://localhost:8081"
+./test_extensions --http-address=0.0.0.0 --http-port=8081  --docroot=. -t 2 -d dc_311-2016.csv.s0311.csv -g ward-2012.geojson
+popd
+fi
